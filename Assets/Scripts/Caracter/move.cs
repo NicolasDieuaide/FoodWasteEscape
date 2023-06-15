e33d0b017247d9573717efdd59e0680e19a4d731
unknown <ndi134333@stud.gibb.ch>
2023-06-15 10:23:42 +0200

    Added Textures

--- Assets/Scripts/Caracter/move.cs
+++ Assets/Scripts/Caracter/move.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine;
+
 public class move : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed = 5f;
+    public float maxTiltAngle = 30f; // Maximum tilt angle in degrees
+
+    private Transform cylinderTransform;
+    private Quaternion initialRotation;
+
+    private void Start()
     {
-        
+        cylinderTransform = GetComponent<Transform>();
+        initialRotation = cylinderTransform.rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, 0f);
+
+        cylinderTransform.Translate(movement * moveSpeed * Time.deltaTime);
+
+        // Reset cylinder rotation
+        cylinderTransform.rotation = initialRotation;
+
+        // Prevent tilting
+        Vector3 eulerRotation = cylinderTransform.rotation.eulerAngles;
+        eulerRotation.z = Mathf.Clamp(eulerRotation.z, -maxTiltAngle, maxTiltAngle);
+        cylinderTransform.rotation = Quaternion.Euler(eulerRotation);
     }
 }
